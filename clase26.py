548b0fc5551da98de507b65f076807b6cf5e8b69
anthonyArellano1923 <163557083+anthonyArellano1923@users.noreply.github.com>
2024-09-11 11:47:06 -0300

    Culminación de ejercicio de clase 24 propuesto por chatGPT

--- clase26.py
+++ clase26.py
@@ -27,5 +27,6 @@ new_product = {
 }
 
 with open('products.csv', mode='a', newline='') as file:
+    file.write('\n')
     csv_writer = csv.DictWriter(file, fieldnames= new_product.keys())
     csv_writer.writerow(new_product)
